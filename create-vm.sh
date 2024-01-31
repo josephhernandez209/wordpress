a844806613c37e302c1441299ce7b4ddccdc9085
Joseph Hernandez <thecoolfoe11@gmail.com>
2024-01-31 10:35:39 -0800

    fix cloudflare deploy

--- create-vm.sh
+++ create-vm.sh
@@ -5,8 +5,24 @@ then
 else
   echo "installing multipass"
   sudo snap install multipass
-  sleep 2
+  sleep 5
 fi
 
-multipass launch --name wordpress -c 4 -m 4G
+if (sudo snap services | grep 'multipass.multipassd' | grep active)
+then 
+  echo "multipass already active"
+else
+  echo "restarting multipass"
+  sudo snap restart multipass.multipassd
+  sleep 5
+fi
+
+if (multipass info wordpress)
+then
+  echo "wordpress vm already exists"
+else 
+  echo "launching wordpress vm"
+  multipass launch --name wordpress -c 4 -m 4G
+fi
+
 multipass transfer wordpress.sh wordpress:. && multipass exec wordpress -- bash wordpress.sh
